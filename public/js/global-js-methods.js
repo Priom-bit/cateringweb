f119d3f7332c0d2d9f40acefe9ee4a364e5415de
Nirob Hasan <hasan53nirob@gmail.com>
2021-04-23 01:04:16 +0600

    add simple website

---
+++ public/js/global-js-methods.js
@@ -0,0 +1,11 @@
+function openNav() 
+{
+	console.log("openNav");
+	document.getElementById("mySidenav").style.width = "30vh";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0vh";
+  document.body.style.backgroundColor = "white";
+}
